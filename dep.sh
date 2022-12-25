893c3e195a164e4e60f1373cfbc8dc73655d6865
hkramyakrishna <114924528+hkramyakrishna@users.noreply.github.com>
2022-12-26 00:30:28 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 install awscli
 {
-    sudo apt update -y
-   sudo apt install awscli -y
+    sudo apt-update -y
+   sudo apt-install awscli -y
     if [ $? -ne 0 ]; then
     echo "awscli not installed"
     fi
 }
 install netcat
 {
-   sudo apt update -y
-   sudo apt install netcat -y
+   sudo apt-update -y
+   sudo apt-install netcat -y
     if [ $? -ne 0 ]; then
     echo "netcat not installed"
     fi
