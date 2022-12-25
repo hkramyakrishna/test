0565572511945b4de16d5defbcea7bae0cd98227
hkramyakrishna <114924528+hkramyakrishna@users.noreply.github.com>
2022-12-25 19:14:49 +0530

    Create dep.sh

---
+++ dep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+install awscli
+{
+    apt-update -y
+    apt-install awscli -y
+    if [ $? -ne 0 ]; then
+    echo "awscli not installed"
+    fi
+}
+install netcat
+{
+    apt-update -y
+    apt-install netcat -y
+    if [ $? -ne 0 ]; then
+    echo "netcat not installed"
+    fi
+}
+
+awscli
+netcat
