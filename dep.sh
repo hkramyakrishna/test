991fffa4e42238dd258a9e1678264c29c5d6754e
hkramyakrishna <114924528+hkramyakrishna@users.noreply.github.com>
2022-12-26 00:39:25 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "hi from git"
+install_awscli
+{
+    sudo apt-update
+   sudo apt-install awscli
+    if [ $? -ne 0 ]; then
+    echo "awscli not installed"
+    fi
+    echo "awscli installed successfully"
+}
+install_netcat
+{
+   sudo apt-update
+   sudo apt-install netcat
+    if [ $? -ne 0 ]; then
+    echo "netcat not installed"
+    fi
+    echo "netcat installed successfully"
+}
+
+awscli
+netcat
