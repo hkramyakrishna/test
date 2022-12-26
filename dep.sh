d73611546fbceae0897fc0646f400eb61db6b5cd
hkramyakrishna <114924528+hkramyakrishna@users.noreply.github.com>
2022-12-26 16:40:57 +0530

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -17,6 +17,24 @@ install_netcat()
     fi
     echo "netcat installed successfully"
 }
+install_tsh() 
+{ 
+   sudo apt update -y
+   sudo curl https://apt.releases.teleport.dev/gpg \
+-o /usr/share/keyrings/teleport-archive-keyring.asc
+source /etc/os-release
+echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
+https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" \
+| sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
+sudo apt-get update
+sudo apt-get install teleport
+if [ $? -ne 0 ]; then
+echo "TSH not installed"
+fi
+echo "TSH installed successfully"
+}
+
 
 install_awscli
 install_netcat
+install_tsh
